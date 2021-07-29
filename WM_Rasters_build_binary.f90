db0eede45a681e12f78b6eb46291f235a5bee2f3
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-07-29 11:18:19 -0500

    convert XYZ asci raster to binary

--- WM_Rasters_build_binary.f90
+++ WM_Rasters_build_binary.f90
@@ -3,20 +3,20 @@
 !   ICM Wetland Morphology Raster Converter
 !                                                  
 !                                                  
-!   Fortran code to convert from binary rasters saved by ICM-Morph to XYZ ascii files
+!   Fortran code to convert to inary rasters saved by ICM-Morph from ascii files
 !   XYZ ascii rasters can be converted to any format using GDAL_translate
 !   
 !   Command line arguments passed into this executable:
 !
-!   1 = xyz_asc_pth : full path to XYZ file that will be saved by this program
+!   1 = xyz_asc_pth : full path to XYZ file that will be passed into this program
 !   2 = x_bin_pth   : full path to binary output file from ICM-Morph that saved array of X-coordinates
 !   3 = y_bin_pth   : full path to binary output file from ICM-Morph that saved array of Y-coordinates
-!   4 = z_bin_pth   : full path to binary output file from ICM-Morph that saved array of raster values (Z) to be mapped
+!   4 = z_bin_pth   : full path to binary output file from ICM-Morph of raster values (Z) that will be saved by this program
 !   5 = dtype       : data type of Z values must be either 'int' or 'flt'
 !   6 = nras_str    : number of raster pixels of dataset, must match size of binary arrays
 !                                                  
 !   Questions: eric.white@la.gov                   
-!   last update: 8/16/2020                          
+!   last update: 7/29/2021
 !                                                     
 !   project site: https://github.com/CPRA-MP      
 !   documentation: http://coastal.la.gov/our-plan  
@@ -70,43 +70,25 @@ program main
     write(*,'(a,a)') 'y file:', trim(adjustL(y_bin_pth))
     write(*,'(a,a)') 'z file:', trim(adjustL(z_bin_pth))
     
-    open(unit=100, file = trim(adjustL(x_bin_pth)),form='unformatted')
-    read(100) x
-    close(100)
-
-    open(unit=101, file = trim(adjustL(y_bin_pth)),form='unformatted')
-    read(101) y
-    close(101)
-    
-    open(unit=102, file = trim(adjustL(z_bin_pth)),form='unformatted')
-    read(102) z
-    close(102)
     
     open(unit=200, file = trim(adjustL(xyz_asc_pth)))
-    write(*,'(A,A)') 'writing output ',trim(adjustL(xyz_asc_pth))
+    write(*,'(A,A)') 'reading input ',trim(adjustL(xyz_asc_pth))
 
     pct = 0
     write(*,'(I3,A)') pct,'%... '
 
     do i=1,nras
-        
-        if(dtype == 'int') then
-            write(200,2000) x(i),y(i),z(i)
-        else if (dtype =='flt') then
-            write(200,2001) x(i),y(i),z(i)
-        end if
+        read(200,*) x(i),y(i),z(i)  
             
         if ( ANY(pct_i == i) ) then
             pct = pct + 10
             write(*,'(I3,A)') pct,'%... '
         end if
-        
     end do
-    
     close(200)
     
-2000 format(I0,',',I0,',',I0)
-2001 format(I0,',',I0,',',F0.4)
-    
-    
+    open(unit=102, file = trim(adjustL(z_bin_pth)),form='unformatted')
+    write(102) z
+    close(102)
+
 end program
